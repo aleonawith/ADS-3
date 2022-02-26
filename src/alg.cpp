9c5548b9d052759d004b6cb5c8a91de81934393e
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-02-26 20:36:09 +0300

    Update alg.cpp
    Смирнова Алена 21-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int count = 0;
-  for (int i = 1; i <= size; i++) {
+  for (int i = 0; i <= size; i++) {
     if (arr[i] == value)
       count++;
   }
